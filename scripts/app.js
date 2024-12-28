dd6536b17a3ddad45b539c4aa412a93f2de4af19
georgeklangat <georgeklangat@gmail.com>
2024-12-28 15:59:16 +0300

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Smart Spending Frontend Loaded");
+    // Add interactivity here, like updating charts or handling form submissions
+  });
+  
